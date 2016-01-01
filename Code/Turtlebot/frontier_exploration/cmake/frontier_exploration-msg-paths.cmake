8aeb00f1942bc24af2eba8e2ee47a8bd32b7ac02
Devesh99 <deveshadlakha@gmail.com>
2016-01-01 22:56:24 +0100

    Second commit: turtlebot code

---
+++ Code/Turtlebot/frontier_exploration/cmake/frontier_exploration-msg-paths.cmake
@@ -0,0 +1,5 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.em
+
+# message include dirs in installspace
+_prepend_path("${frontier_exploration_DIR}/.." "msg;msg" frontier_exploration_MSG_INCLUDE_DIRS UNIQUE)
+set(frontier_exploration_MSG_DEPENDENCIES geometry_msgs;std_msgs;actionlib_msgs;move_base_msgs;visualization_msgs)
